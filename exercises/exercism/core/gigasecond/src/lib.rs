4c3b4de38daaadf337a2507486340aa7e7383de0
Gaurav Agarwal <algogrit@gmail.com>
2021-05-22 13:16:16 +0530

    Adding exercises from exercism.io.

---
+++ exercises/exercism/core/gigasecond/src/lib.rs
@@ -0,0 +1,7 @@
+extern crate chrono;
+use chrono::{Duration, DateTime, Utc};
+
+// Returns a Utc DateTime one billion seconds after start.
+pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
+    return start + Duration::seconds(1000000000);
+}
