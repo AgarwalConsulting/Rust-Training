dabed0ac51ca38fa9644ce35f0395b2ee5e23caf
Gaurav Agarwal <algogrit@gmail.com>
2021-05-22 18:12:03 +0530

    Rewriting the code in functional style.

--- sample-apps/minigrep/src/main.rs
+++ sample-apps/minigrep/src/main.rs
@@ -2,14 +2,15 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    // let args: Vec<String> = env::args().collect();
+    let args: env::Args = env::args();
 
     // let query = &args[1];
     // let filename = &args[2];
 
     // let (query, filename) = parse_config(&args);
 
-    let config = minigrep::Config::new(&args).unwrap_or_else( |err| {
+    let config = minigrep::Config::new(args).unwrap_or_else( |err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
